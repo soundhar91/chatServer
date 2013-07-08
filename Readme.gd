6cdabff21037775000c898422c019130aa991f86
Soundar <svelliangiri@groupon.com>
2013-07-09 01:23:34 +0530

    Readme is added

---
+++ Readme.gd
@@ -0,0 +1,5 @@
+Make sure you installed nodejs in your system.After you cloned the repo do the following steps
+
+1. node server.js
+2. <open another terminal tab> nc localhost 5001
+3. You can enter your username and start chatting.
